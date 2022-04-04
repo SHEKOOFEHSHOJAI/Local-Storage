d23ebd427f938dc5c9068d8f4c86a6d978d3554d
SHEKOOFEHSHOJAI <shekoofehshojai.78@gmail.com>
2022-04-04 12:00:07 -0700

    shekoofeh-shojai-classA

---
+++ cookies/script.js
@@ -0,0 +1,6 @@
+localStorage.setItem('name','shekoofeh')
+console.log( localStorage.getItem(name))
+localStorage.removeItem('name')
+localStorage.setItem("name", "jone"); 
+
+document.cookie='last=shojai; Expires='+new Date(2122,0,1).toUTCString()
